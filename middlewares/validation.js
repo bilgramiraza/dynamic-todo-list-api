25db14251f1e8d43ca1d8d896f946d47a754d38d
bilgramiraza <bilgramiraza@yahoo.com>
2023-09-15 16:59:26 +0530

    Setup basic BoilerPlate for Validation

---
+++ middlewares/validation.js
@@ -0,0 +1,21 @@
+const { body, validationResult } = require('express-validator');
+
+function validationObject(req, res, next){
+  //Create an ErrorHandling Object in the format of 
+  //{[param]:[msg]}
+  //Where 'params' is the input name which failed 
+  //validation and msg is the Error Message generated for it
+
+  const errorObject = validationResult(req).formatWith(({msg})=>msg).mapped();
+  if(Object.keys(errorObject).length>0) req.errorObject = errorObject;
+  return next();
+}
+
+const [form]Validation = [
+  body('[fieldName]', '/*Base Error Msg*/').trim().escape(),
+  validationObject,
+];
+
+module.exports={
+  [form]Validation,
+};

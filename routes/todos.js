224c9f3b1f47120f2b3e7351b0dacce9339be7f9
bilgramiraza <bilgramiraza@yahoo.com>
2023-09-16 00:18:51 +0530

    Planning REST Routes for the application

--- routes/todos.js
+++ routes/todos.js
@@ -5,12 +5,14 @@ const todoController = require('../controllers/todoController');
 const { todoValidation } = require('../middlewares/validation');
 
 /*TODOS API Routes*/
-router.get('/', todoValidation, todoController.);
+router.get('/', todoController.getAllTodos);
+router.get('/:id', todoController.getTodoById);
 
-router.post('/', todoValidation, todoController.);
+router.post('/', todoValidation, todoController.addTodo);
 
-router.patch('/', todoValidation, todoController.);
+router.post('/:id', todoController.toggleTodoStatus);
+router.patch('/:id', todoValidation, todoController.ModifyTodo);
 
-router.delete('/', todoValidation, todoController.);
+router.delete('/:id', todoController.deleteTodo);
 
 module.exports = router;

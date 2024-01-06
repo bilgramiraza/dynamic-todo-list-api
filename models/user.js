a1582a9cf7ac62a946ee26f742f3e0ced5900f0c
bilgramiraza <bilgramiraza@yahoo.com>
2024-01-06 12:05:22 +0530

    added encryption for user passwords

---
+++ models/user.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: { type:String, required:true },
+  password: { type:String, required:true },
+  email: { type:String, required:true },
+},{ timestamps: true });
+
+userSchema.pre('save',async function(next){
+  if(!this.isModified('password'))  return next();
+
+  try{
+    const hashedPassword = await bcrypt.hash(this.password,10);
+    this.password = hashedPassword;
+    return next();
+  }catch(err){
+    return next(err);
+  }
+}); 
+
+userSchema.pre('findOneAndUpdate',async function(next){
+  const data = this.getUpdate();
+
+  if(!data && !data.password) return next();
+  
+  try{
+    const hashedPassword = await bcrypt.hash(data.password,10);
+    data.password = hashedPassword;
+    return next();
+  }catch(err){
+    return next(err);
+  }
+});
+
+userSchema.methods.comparePassword = async function(inputPassword){
+  return await bcrypt.compare(inputPassword,this.password);
+}
+
+module.exports = mongoose.model('user', userSchema);

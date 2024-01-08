d37ba68c75ecb3bde262707b0cc508fe4fbb628e
bilgramiraza <bilgramiraza@yahoo.com>
2024-01-08 11:03:02 +0530

    Added User Field to the Todo DB

--- models/todo.js
+++ models/todo.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const todoSchema = new Schema({
   title: { type:String, required:true },
   status: { type:Boolean, default:false, required:true },
+  user: { type:Schema.Types.ObjectId, ref:'user', required:true}
 },{ timestamps: true });
 
 // todoSchema.virtual('').get(() => {

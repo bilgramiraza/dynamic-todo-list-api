dbf20b158972bc613acd1403cd675fedc2cd8513
bilgramiraza <bilgramiraza@yahoo.com>
2024-01-10 11:32:59 +0530

    Added Users route to the api route and Completed the Users Controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,6 @@
 const User = require('../models/user'); 
 
 const login = async (req, res) =>{
-  if(req.errorObject) 
-    return res.status(400).send({ message:req.errorObject });
-
   const { username, password } = req.body;
   try{
     const foundUser = await User.findOne({ username }).exec();
@@ -15,47 +12,50 @@ const login = async (req, res) =>{
       return res.status(401).send({ message:'Invalid Password' });
 
     const token = foundUser.genAuthToken();
-    return res.status(200).json({ message:'User Log In Successful', token });
+    return res.status(200).json({ message:'User Login Successful', token });
   }catch(err){
     return res.status(500).send(err);
   }
 };
 
 const register = async (req, res)=>{
-  if(req.errorObject)
-    return res.status(400).send({ message:req.errorObject });
   try{
-
+    const user = new User(req.body);
+    await user.save();
+    return res.status(201).json({ message:'Registration Successful' });
   }catch(err){
     return res.status(500).send(err);
   }
 };
 
 const currentUser = async (req, res)=>{
-  if(req.errorObject)
-    return res.status(400).send({ message:req.errorObject });
   try{
-
+    const foundUser = await User.findById(req.user.id).select('username email').lean().exec();
+    const user = {
+      username:foundUser.username,
+      email:foundUser.email,
+    };
+    return res.status(200).send({ user });
   }catch(err){
     return res.status(500).send(err);
   }
 };
 
 const modifyUser = async (req, res)=>{
-  if(req.errorObject)
-    return res.status(400).send({ message:req.errorObject });
   try{
-
+    const user = new User(req.body);
+    await User.findByIdAndUpdate(req.user.id, user, { new:true });
+    return res.status(201).json({ message:'User Info Updated' });
   }catch(err){
     return res.status(500).send(err);
   }
 };
 
 const deleteUser = async (req, res)=>{
-  if(req.errorObject)
-    return res.status(400).send({ message:req.errorObject });
   try{
-
+    const deletedUser = await User.findByIdAndDelete(req.user.id);
+    if(!deletedUser)  res.status(404).send({ message:'User Not Found' });
+    return res.status(204).json({ message:'User Deletion Successful' });
   }catch(err){
     return res.status(500).send(err);
   }
--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const todosRouter = require('./todos');
+const usersRouter = require('./users');
 
 router.use('/todos', todosRouter);
+router.use('/users', usersRouter);
 
 module.exports = router;
